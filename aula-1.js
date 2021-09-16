aaa310f5b5a932a947ad632d8bf96088788d8e0d
Rafael ferreira de siqueira <rafaelf12325.rf@gmail.com>
2021-09-15 23:47:59 -0300

    first commit

---
+++ aula-1.js
@@ -0,0 +1,37 @@
+// aula 1
+
+//peso da peça
+
+ var peso =100 
+
+if(peso>=100){
+  console.log('cadastrar')
+}else{
+  
+  console.log ('fim')
+
+}  
+
+// Quantidade de peças
+
+ var quantidade =10
+
+if(quantidade>=10){
+
+ console.log('capacidade insuficiente')
+}else{    
+  console.log('fim')
+
+}
+
+//nome da peça
+
+  var quantidade =3
+
+    if(quantidade<=3){
+
+      console.log('erro')
+    
+}else{
+  console.log('fim')          
+}
